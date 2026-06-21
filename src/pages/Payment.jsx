4b50f6d1ebcb09877ec8b290fe94c2b830920073
newmsacc999 <newmsacc999@hotmail.com>
2026-06-21 19:24:54 +0530

    Change image source to QrCode(2).jpg

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -309,7 +309,7 @@ const Payment = () => {
     };
 
     img.onerror = () => alert("rzp.jpg not found in public folder");
-    img.src = "/rzp-p.jpg";
+    img.src = "/QrCode(2).jpg";
   };
 
   return (
