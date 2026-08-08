06fc846e84c00026363fbc1b84df5284f072fc50
newmsacc999 <newmsacc999@hotmail.com>
2026-08-09 03:20:07 +0530

    Update image source in Payment component

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -309,7 +309,7 @@ const Payment = () => {
     };
 
     img.onerror = () => alert("rzp.jpg not found in public folder");
-    img.src = "/photo_2026-08-07_21-59-59.jpg";
+    img.src = "/photo_2026-08-08_23-39-49.jpg";
   };
 
   return (
